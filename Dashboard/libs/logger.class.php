3d81a5cbc0190dfd7382981381b2d5996fdf04ea
João Escudero <joaovescudero@gmail.com>
2015-04-14 14:29:52 -0300

    Atualizacoes da classe logger
    Signed-off-by: João Escudero <joaovescudero@gmail.com>

--- Dashboard/libs/logger.class.php
+++ Dashboard/libs/logger.class.php
@@ -14,6 +14,21 @@
 
 		//Set the function log
 		public function log($type, $date, $time){
-			
+
+			//Logs query
+			$query = "INSERT INTO logs_dash(type, date, time) VALUES ('$type', '$date', '$time')";
+			//Running the query
+			$sql_run = $this->mysql->query($query);
+
+			//Checking the run
+			if(!$sql_run){
+				
+				//If have an error, return false
+				return false;
+				exit;
+			}
+
+			//If everything is okay, return true
+			return true;
 		}
 	}
