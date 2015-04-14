bbacf018f17cd886dcd221ee83e62aee78d75ccc
João Escudero <joaovescudero@gmail.com>
2015-04-13 23:37:27 -0300

    Comeco da classe logger
    Signed-off-by: João Escudero <joaovescudero@gmail.com>

---
+++ Dashboard/libs/logger.class.php
@@ -0,0 +1,19 @@
+<?php
+	//Logger class archive
+	//Create by: Joao Escudero <joaovescudero@gmail.com>
+	
+	//Require configuration and database file
+	require_once("conf.php");
+	require_once("database.conf.php");
+
+	//Create class Logger
+	class Logger {
+
+		//Set the var mysql
+		private $mysql = $mysql;
+
+		//Set the function log
+		public function log($type, $date, $time){
+			
+		}
+	}
