8597ceeceb6b936b4016d8caaf9a7f74aabbc84d
João Escudero <joaovescudero@gmail.com>
2015-04-13 23:15:26 -0300

    Arquivo de configuracao basica
    Signed-off-by: João Escudero <joaovescudero@gmail.com>

---
+++ Dashboard/libs/conf.php
@@ -0,0 +1,20 @@
+<?php
+	//Configuration archive
+	//Create by: Joao Escudero <joaovescudero@gmail.com>
+	
+	//Basic configuration
+	//Game name
+	$name = "Test game";
+	//Description
+	$description = "A simple test web-based game!";
+
+
+	//Mysql configuration
+	//Host
+	$host = "localhost";
+	//User
+	$user = "root";
+	//Password
+	$pass = "";
+	//Database
+	$db = "game";
